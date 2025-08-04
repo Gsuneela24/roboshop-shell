d15abfc88dca3ae386fa6c7d7277d01758b286dd
suneelagudipudi <gsuneeela24@gmail.com>
2025-08-03 20:58:50 -0400

    Roboshop Project

--- cart.sh
+++ cart.sh
@@ -1,11 +1,4 @@
 component_name =cart
 source common.sh
 nodejs
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
 systemd_setup
--- common.sh
+++ common.sh
@@ -4,7 +4,15 @@ systemd_setup(){
   systemctl start ${component_name}
 }
 nodejs(){
+  cp ${component_name}.service /etc/systemd/system/${component_name}.service
   dnf module disable nginx -y
   dnf module enable nginx:1.24 -y
   dnf install nginx -y
+  useradd roboshop
+  mkdir /app
+  curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/${compoent_name}-v3.zip
+  cd /app
+  unzip /tmp/${component_name}.zip
+  cd /app
+  npm install
 }
